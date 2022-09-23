03b51f7bb4e6c0db35e5473b4303e6f957131b5a
Neeraj Sharma <nsharma@confluent.io>
2022-09-23 15:26:43 +0530

    Add secret protection support and enahanced customer properties

--- discovery/service/zookeeper.py
+++ discovery/service/zookeeper.py
@@ -49,7 +49,7 @@ class ZookeeperServicePropertyBaseBuilder(AbstractPropertyBuilder):
         self.__build_service_properties(service_properties)
 
         # Add custom properties
-        self.__build_custom_properties(service_properties, self.mapped_service_properties)
+        self.__build_custom_properties(host_service_properties, self.mapped_service_properties)
 
         # Build Command line properties
         self.__build_runtime_properties(hosts)
@@ -68,15 +68,27 @@ class ZookeeperServicePropertyBaseBuilder(AbstractPropertyBuilder):
                 result = func(self, service_properties)
                 self.update_inventory(self.inventory, result)
 
-    def __build_custom_properties(self, service_properties: dict, mapped_properties: set):
+    def __build_custom_properties(self, host_service_properties: dict, mapped_properties: set):
 
-        group = "zookeeper_custom_properties"
+        custom_group = "zookeeper_custom_properties"
         skip_properties = set(FileUtils.get_zookeeper_configs("skip_properties"))
+<<<<<<< HEAD
         self.build_custom_properties(inventory=self.inventory,
                                      group=group,
                                      skip_properties=skip_properties,
                                      mapped_properties=mapped_properties,
                                      service_properties=service_properties)
+=======
+
+        # Get host server properties dictionary
+        _host_service_properties = dict()
+        for host in host_service_properties.keys():
+            _host_service_properties[host] = host_service_properties.get(host).get(DEFAULT_KEY)
+        self.build_custom_properties(inventory=self.inventory, group=self.service.value.get('group'),
+                                     custom_properties_group_name=custom_group,
+                                     host_service_properties=_host_service_properties, skip_properties=skip_properties,
+                                     mapped_properties=mapped_properties)
+>>>>>>> 5378ad6d (Add secret protection support and enahanced customer properties)
 
     def __build_runtime_properties(self, hosts: list):
         # Build Java runtime overrides
